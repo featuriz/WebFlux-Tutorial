9ed69f8a359a03d415081e20273003c9b306be89
SudhakarK <featuriz@gmail.com>
2022-10-17 08:45:42 +0530

    INIT

---
+++ ReactiveSpringWebFlux/src/main/java/com/fz/rsw/ReactiveSpringWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.fz.rsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringWebFluxApplication.class, args);
+	}
+
+}
---
+++ ReactiveSpringWebFlux/src/test/java/com/fz/rsw/ReactiveSpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fz.rsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
